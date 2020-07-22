4c215bb152e49c78f5254e8894f782b19b4ac718
Eunjin Kim <jagpavv@gmail.com>
2020-07-22 16:57:35 +0200

    Separted weather service from VC

---
+++ Weather/WeatherSearchService.swift
@@ -0,0 +1,42 @@
+import Foundation
+
+let OpenWeatherAPIKey = "4c8b3b461a4559a8ac0c397de4b3aaaf"
+
+class WeatherSearchService {
+
+  private var baseURL = "http://api.openweathermap.org"
+  private var path = "/data/2.5/group"
+
+  private var jsonDecoder: JSONDecoder = {
+    let decoder = JSONDecoder()
+    decoder.keyDecodingStrategy = .convertFromSnakeCase
+    return decoder
+  }()
+
+  func getWeather(cityIDString: String, completion: @escaping (Result<WeatherResult, Error>) -> Void) {
+
+    let cityIDString = "524901,703448,2643743"
+
+    guard var components = URLComponents(string: baseURL + path) else { return }
+    components.queryItems = [URLQueryItem(name: "id", value: cityIDString),
+                             URLQueryItem(name: "units", value: "metric"),
+                             URLQueryItem(name: "APPID", value: OpenWeatherAPIKey)]
+
+    guard let url = components.url else { return }
+    let request = URLRequest(url: url)
+
+    URLSession.shared.dataTask(with: request) { (data, response, error) in
+      if let error = error {
+        completion(.failure(error))
+      }
+
+      guard let data = data else { return }
+      do {
+        let decoded = try self.jsonDecoder.decode(WeatherResult.self, from: data)
+        completion(.success(decoded))
+      } catch {
+        completion(.failure(error))
+      }
+    }.resume()
+  }
+}
